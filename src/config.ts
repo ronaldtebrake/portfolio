13a6f48d8ab4de5521de31d6b7ea043966c1a220
ronaldtebrake <ronald@goalgorilla.com>
2022-12-10 12:20:10 +0100

    Configure global data, like site title and description

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Ronald te Brake';
+export const SITE_DESCRIPTION = 'A developer blog, playground and showcase. Showcasing everything I am learning about Drupal, PHP and more!';
